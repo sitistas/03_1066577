d3e8153fe76cc0593691e775e53dd44cb3863b79
sitistas <kon.sitistas@gmail.com>
2022-05-14 17:47:14 +0300

    First commit

---
+++ src/main/java/MqttService.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.UUID;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+
+
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MqttService {
+
+}
